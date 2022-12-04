1821fd30ce9d86fc9f1f552a86277538ba917f32
gaurav <gauravpal541@gmail.com>
2022-12-05 04:22:20 +0530

    push

--- app.js
+++ app.js
@@ -16,12 +16,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(express.static("public"));
 
-if (process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'staging') {
-  app.use(express.static('client/build'));
-  app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname + '/client/build/index.html'));
-  });
- }
 
 app.use(
   session({
